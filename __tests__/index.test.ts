2e263843e6156564664590848ee2d3a63f196af1
Remo de Oliveira Gresta <remoogg@gmail.com>
2023-10-26 00:05:08 -0300

    first commit

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -2,16 +2,16 @@
  * Unit tests for the action's entrypoint, src/index.ts
  */
 
-import * as main from '../src/main'
+import { run } from '../src/main'
 
 // Mock the action's entrypoint
-const runMock = jest.spyOn(main, 'run').mockImplementation()
 
 describe('index', () => {
-  it('calls run when imported', async () => {
-    // eslint-disable-next-line @typescript-eslint/no-require-imports
-    require('../src/index')
-
-    expect(runMock).toHaveBeenCalled()
-  })
+  test.concurrent(
+    'calls run when imported',
+    async () => {
+      await run()
+    },
+    1000000
+  )
 })
